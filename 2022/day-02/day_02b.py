60ffb65984543e4a75c79eb8e7616eb2cf67fd61
nveretennicov <121759872+nveretennicov@users.noreply.github.com>
2023-01-02 13:30:15 +0000

    Add files via upload

---
+++ 2022/day-02/day_02b.py
@@ -0,0 +1,42 @@
+scoring = {
+    # Draw scenarios
+    ('X', 'A'): 3,
+    ('Y', 'B'): 3,
+    ('Z', 'C'): 3,
+    # Win scenarios
+    ('X', 'C'): 6,
+    ('Y', 'A'): 6,
+    ('Z', 'B'): 6,
+}
+
+def choose_pick(opponent_pick, round_outcome):
+    loss_responses = {'A': 'Z', 'B': 'X', 'C': 'Y'}
+    draw_responses = {'A': 'X', 'B': 'Y', 'C': 'Z'}
+    win_responses = {'A': 'Y', 'B': 'Z', 'C': 'X'}
+
+    if round_outcome == 'X':
+        pick = loss_responses[opponent_pick]
+    elif round_outcome == 'Y':
+        pick = draw_responses[opponent_pick]
+    else:
+        pick = win_responses[opponent_pick]
+    return pick
+
+def calculate_score(opponent_pick, your_pick):
+    shape_points = {'X': 1, 'Y': 2, 'Z': 3}
+    score = shape_points[your_pick] + scoring.get((your_pick, opponent_pick), 0)
+    return score
+
+def main():
+    with open('day_02_input.txt', 'r') as puzzle_input:
+        rounds = puzzle_input.read().split('\n')
+        total_score = 0
+        for round in rounds:
+            opponent_pick, round_outcome = round.split(' ')
+            your_pick = choose_pick(opponent_pick, round_outcome) 
+            total_score += calculate_score(opponent_pick, your_pick)
+            
+        print(total_score)
+
+if __name__ == '__main__':
+    main()
